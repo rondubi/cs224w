346e579aa0c886c2c6812bf33bc0ae4557d1dd61
h3seas0n <shizhe.he6@gmail.com>
2023-12-06 20:48:06 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+from typing import List
 
 # helper function to read h5 file to a dictionary
 def read_h5(file_path):
@@ -18,3 +19,29 @@ def read_h5(file_path):
                 'embeddings': sample_group['embeddings'][()]
             }
     return sample_data
+
+def topK_accuracy(true_label : str, pred_labels : List[str], k : int):
+    # pred_labels must be sorted in descending order of confidence!
+    if true_label in pred_labels[:k]:
+        return 1
+    else:
+        return 0
+    
+def topK_accuracy_all(true_labels : List[str], pred_labels : List[str], k : int):
+    correct = 0
+    for i in range(len(true_labels)):
+        correct += topK_accuracy(true_labels[i], pred_labels[i], k)
+    return correct / len(true_labels)
+
+def precision_at_k(true_label : str, pred_labels : List[str], k : int):
+    # pred_labels must be sorted in descending order of confidence!
+    if true_label in pred_labels[:k]:
+        return 1
+    else:
+        return 0
+
+def NDCG(true_label : str, pred_labels : List[str], k : int):
+    if true_label in pred_labels[:k]:
+        return 1 / np.log2(pred_labels.index(true_label) + 2)
+    else:
+        return 0
